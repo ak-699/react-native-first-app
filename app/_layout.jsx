1df5b8a08ac454ffefb3e5f018c4b677cd5d5805
Abhishek Kumar <kumar699.abhishek@gmail.com>
2024-08-28 23:53:03 +0530

    Add clerk boilerplate code

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,5 +1,40 @@
+import { ClerkProvider } from "@clerk/clerk-expo";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+import * as SecureStore from "expo-secure-store";
+
+const tokenCache = {
+  async getToken(key) {
+    try {
+      const item = await SecureStore.getItemAsync(key);
+      if (item) {
+        console.log(`${key} was used 🔐 \n`);
+      } else {
+        console.log("No values stored under key: " + key);
+      }
+      return item;
+    } catch (error) {
+      console.error("SecureStore get item error: ", error);
+      await SecureStore.deleteItemAsync(key);
+      return null;
+    }
+  },
+  async saveToken(key, value) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (err) {
+      return;
+    }
+  },
+};
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
+
+if (!publishableKey) {
+  throw new Error(
+    "Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env"
+  );
+}
 
 export default function RootLayout() {
   useFonts({
@@ -8,14 +43,16 @@ export default function RootLayout() {
     "outfit-bold": require("../assets/fonts/Outfit-Bold.ttf"),
   });
   return (
-    <Stack>
-      <Stack.Screen name="index" />
-      <Stack.Screen
-        name="login/index"
-        options={{
-          headerShown: false,
-        }}
-      />
-    </Stack>
+    <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
+      <Stack>
+        <Stack.Screen name="index" />
+        <Stack.Screen
+          name="login/index"
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack>
+    </ClerkProvider>
   );
 }
